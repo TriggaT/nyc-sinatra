9e72319251d5d08de14a47a5f82bb49902855528
Trayshawn <traytwebb14@yahoo.com>
2020-12-28 15:35:59 -0600

    commit tree f8db02fb983789c84e94843de6ef1c8d09aaeba5

--- app/controllers/landmarks_controller.rb
+++ app/controllers/landmarks_controller.rb
@@ -1,3 +1,60 @@
 class LandmarksController < ApplicationController
   # add controller methods
+  get '/landmarks' do
+    @landmarks = Landmark.all
+    erb :'/landmarks/index'
+  end
+
+  post '/landmarks' do 
+    @landmark = Landmark.create(params[:landmark])
+    if !params["figure"]["name"].empty?
+      Figure.create(name: params["figure"]["name"]).landmarks << @landmark
+    end
+  
+    @Figure.save
+
+    
+
+    redirect "/landmarks/#{@landmark.id}"
+
+  end 
+
+  get '/landmarks/new' do 
+    erb :'/landmarks/new'
+  end
+
+  get '/landmarks/:id' do 
+    @landmark = Landmark.find(params[:id])
+
+    erb :'/landmarks/show'
+  end
+
+  get '/landmarks/:id/edit' do
+    @landmark = Landmark.find(params[:id])
+
+    
+    erb :'/landmarks/edit'
+  end 
+
+  patch "/landmarks/:id" do
+    @landmark = Landmark.find(params[:id])
+
+    @landmark.update(params[:landmark])
+    if !params["figure"]["name"].empty?
+      Figure.create(name: params["figure"]["name"]).landmarks << @landmark
+    end
+
+    binding.pry
+    
+  
+    @landmark.save
+
+
+
+
+    
+
+    redirect "/landmarks/#{@landmark.id}"
+  end 
+
 end
